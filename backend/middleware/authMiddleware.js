979c410cc6eb0735a03e3a13c3a8fd318be774c2
rakshit1862005 <95012114+rakshit1862005@users.noreply.github.com>
2026-01-14 23:35:06 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
